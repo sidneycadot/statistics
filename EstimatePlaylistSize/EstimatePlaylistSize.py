0250a1375264177f03070fc1ed1bbf8e5bbd55ce
Sidney Cadot <sidney@jigsaw.nl>
2016-03-23 08:52:03 +0100

    Added EstimatePlaylistSize.py

--- EstimatePlaylistSize/EstimatePlaylistSize.py
+++ EstimatePlaylistSize/EstimatePlaylistSize.py
@@ -23,9 +23,41 @@
 #
 # Our model states that all balls have an identical probability of being drawn.
 #
-# "dd" vectors
-# ------------
-
+# dd-tuples
+# ---------
+#
+# In the code below, have use is made of 'dd-tuples'. "dd" stands for "duplicate distribution".
+# Given a sequence of draws from a vase, the dd-tuple summarizes the observed balls in terms of duplicates seen.
+#
+# As a definition: the i'th element of the tuple counts the number of balls that were seen (i + 1) times during
+# the sequence of draws.
+#
+# For example, suppose we draw the following sequence of balls: [ C, A, C, C, D, B, C, B ]
+#
+#   We observed 2 balls only once: A and D.
+#   We observed 1 ball twice: B
+#   We observed 0 balls three times.
+#   We observed 1 ball four times: C.
+#
+# These observations are summarized using the dd-tuple (2, 1, 0, 1).
+#
+# Note that adding zeroes to the end of a dd-tuple does not alter its meaning. By convention, we end the tuple
+#   at its last non-zero entry.
+#
+# From a given dd-tuple, we can extract two important pieces of information:
+#
+#   (1) sum(dd)                      equals the number of different balls observed, a.k.a. num_unique.
+#   (2) dd . [1, 2, 3, 4, 5, 6, ...] i.e, the sum of dd values multiplied by the increasing integer range starting at 1,
+#                                         equals the number of balls observed in total (including duplicates), a.k.a. num_draws.
+#
+# dd-distributions
+# ----------------
+#
+# For a given experimental setup where we know the number of balls in the vase (num_balls) and the number of balls to be drawn (num_draws),
+# there are (num_balls ** num_draws) possible equiprobable sequences of drawn balls.
+#
+# If we determine the d-tuple for each of the possible sequences, we will see that any given dd-tuple can be obtained in multiple ways.
+# The number of draw sequences that give rise to a certain dd-tuple is directly proportional to its probability.
 
 import itertools, collections, time
 import numpy as np
@@ -58,34 +90,31 @@ from matplotlib import pyplot as plt
 #     390625 total
 
 def brute_force(num_balls, num_draws):
-    """ Enumerate all possible sequences of 'num_draws' draws, from a vase with 'num_balls' balls,
-        with replacement.
-        Summarize each sequence as a 'dd' vector; and count the different vectors as they occur.
-
-        A 'dd' vector is defined as follows: dd[i] counts the number of balls that were seen
-        precisely (i + 1) times. E.g., in an experiment with 8 draws, a dd vector of [2, 1, 0, 1]
-        means that 2 balls were seen once, 1 ball was seen twice, and 1 ball was seen four times.
+    """ Enumerate all possible sequences of 'num_draws' draws, from a vase containing 'num_balls' balls,
+        with replacement, by brute force.
+        Summarize each draw sequence as a dd-tuple, and count the different dd-tuples as they occur.
     """
     balls = list(range(num_balls))
 
-    distributions = collections.Counter()
+    dd_distribution = collections.Counter()
 
     for draw in itertools.product(*[balls for d in range(num_draws)]):
         count_duplicates = collections.Counter(collections.Counter(draw).values())
-        distribution = tuple(count_duplicates[i] for i in range(1, 1 + max(count_duplicates)))
-        distributions[distribution] += 1
+        dd = tuple(count_duplicates[i] for i in range(1, 1 + max(count_duplicates)))
+        dd_distribution[dd] += 1
 
-    return distributions
+    return dd_distribution
 
-def calc_count(dd, n):
-    """ Given a particular 'dd' vector and a value 'n' equal to the number of different balls in the vase,
-        This function returns the number of different draws that result in the 'dd' vectors given.
+def calc_count(dd, num_balls):
+    """ Given a particular dd-tuple and num_balls (the number of different balls in the vase),
+          return the number of different draws that result in the dd-tuple given.
     """
 
-    us = np.sum(dd)
-    ts = np.dot(dd, np.arange(1, len(dd) + 1))
+    num_unique = np.sum(dd)                             # number of unique balls seen, according to the dd-tuple.
+    num_draws  = np.dot(dd, np.arange(1, len(dd) + 1))  # number of balls seen (a.k.a., number of draws), according to the dd-tuple.
 
-    return math.factorial(ts) // np.product([math.factorial(d) * math.factorial(i) ** d for (i, d) in enumerate(dd, 1)]) * (math.factorial(n) // math.factorial(n - us))
+    return math.factorial(num_draws) // np.product([math.factorial(d) * math.factorial(i) ** d for (i, d) in enumerate(dd, 1)]) * \
+                  (math.factorial(num_balls) // math.factorial(num_balls - num_unique))
 
 def test_calc_count():
     """ Verify that the result of calc_count() is identical to the count we obtain from
@@ -120,21 +149,61 @@ def test_calc_count():
 
                 assert total_count == (num_balls ** num_draws)
 
-
-def enumerate_dd(sofar, remaining):
-    """ This code is incorrect!
-        It generates solutions even when the number of different balls is too low.
+def enumerate_dd_rec(sofar, balls_unseen, draws_remaining):
+    """ Generate all possible dd-tuples recursively.
     """
-    if remaining == 0:
-        yield sofar
+    if balls_unseen >= 0 and draws_remaining == 0:
+        yield tuple(sofar)
+
     curr = len(sofar) + 1
-    if remaining < curr:
+
+    if draws_remaining < curr:
         return
+
     p = 0
-    while remaining >= 0:
-        yield from enumerate_dd(sofar + [p], remaining)
+    while balls_unseen >= 0 and draws_remaining >= 0:
+        yield from enumerate_dd_rec(sofar + [p], balls_unseen, draws_remaining)
         p += 1
-        remaining -= curr
+        balls_unseen -= 1
+        draws_remaining -= curr
+
+def enumerate_dd(num_balls, num_draws):
+    """ Generate all possible dd-tuples.
+    """
+    yield from enumerate_dd_rec([], num_balls, num_draws)
+
+def fast_enumerate(num_balls, num_draws):
+    """ This is functionally equivalent to br
+    """
+    dd_distribution = collections.Counter()
+    for dd in enumerate_dd(num_balls, num_draws):
+        dd_distribution[dd] = calc_count(dd, num_balls)
+    return dd_distribution
+
+def test_fast_enumerate():
+    for num_balls in range(0, 8):
+        for num_draws in range(1, 8):
+            bf = brute_force(num_balls, num_draws)
+            fe = fast_enumerate(num_balls, num_draws)
+            print(num_balls, num_draws)
+            assert bf == fe
+
+def examine():
+    for num_balls in range(30):
+        dd_distribution = fast_enumerate(num_balls, 3)
+        cLess = 0
+        cEqual = 0
+        cMore = 0
+        refcount = dd_distribution[(3, )]
+        for dd in dd_distribution:
+            if dd_distribution[dd] < refcount:
+                cLess += calc_count(dd, num_balls)
+        print(num_balls, cLess)
+
+def check_tree():
+    dd_distribution = fast_enumerate(6, 9)
+    for dd in dd_distribution:
+        print(dd, dd_distribution[dd])
 
 def montecarlo(dd, num_balls, num_repeats):
 
@@ -171,21 +240,19 @@ def drive_monte_carlo():
     plt.show()
 
 def calc_ts_counts(ts, n):
-    for dd in enumerate_dd([], ts):
+    for dd in enumerate_dd([], 8, 10):
         count = calc_count(dd, n)
         print(count, dd)
 
 def main():
-    test_calc_count()
-    #drive_monte_carlo()
+
+    #for dd in enumerate_dd([], ts):
+    #    count = calc_count(dd, n)
+
+    #test_calc_count()
+    #test_fast_enumerate()
+
+    check_tree()
 
 if __name__ == "__main__":
     main()
-    #main()
-    #print(calc_count([2,1,0,1], 5))
-    #print(H(11.3))
-    #MaximumLikelihoodEstimator([1974,295,17,2])
-    #for n in range(1, 20):
-    #    count = len(list(enumerate_dd([], n)))
-    #    print(n, count)
-    #calc_ts_counts(10, 30)
